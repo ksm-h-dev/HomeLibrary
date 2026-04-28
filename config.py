7ac15a0d588d83070c7a46f5d7316a83df5b99bd
Developer <dev@library.local>
2026-04-28 09:27:06 +0300

    Restore missing icons (cleanup.png, wipe.png, transfer.png) and config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BOOKS_DIR = os.getenv("LIBRARY_BOOKS_DIR", "H:/Book/")
+DATABASE_URL = os.getenv("LIBRARY_DB", "library.db")
+SERVER_HOST = os.getenv("LIBRARY_HOST", "0.0.0.0")
+SERVER_PORT = int(os.getenv("LIBRARY_PORT", "8000"))
+
+DEFAULT_SOURCE_PATH = os.getenv("LIBRARY_DEFAULT_SOURCE", "H:/Book")
+SHOW_WELCOME = os.getenv("LIBRARY_SHOW_WELCOME", "true")
+
+SUPPORTED_FORMATS = ["pdf", "djvu", "rar", "zip", "rtf"]
+SUPPORTED_METADATA_EXT = ["txt", "json"]
+COVER_EXTENSIONS = ["jpg", "jpeg", "png", "gif", "webp", "bmp", "tiff"]
